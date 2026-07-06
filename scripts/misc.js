650c4434d49e950cb4e190399d48c2af09a6f187
australiaembassyxs-byte <australiaembassyxs@gmail.com>
2026-07-06 11:46:10 +0400

    Add files via upload

---
+++ scripts/misc.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $("div#demo3").jContent({
+        orientation: 'horizontal',
+        easing: "easeOutCirc",
+        duration: 500,
+        auto: true,
+        //auto: false,
+        pause_on_hover: true,
+        direction: 'next',
+        width: 280,
+        height: 130,
+        pause: 3500
+    });
+});
+   
+
+   
+
+
+
+
+
+
+
+
+   
+   
+  
+
